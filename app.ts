789b43ca28e24a12d347f380a8331c06cb3b1ccb
Ben Matthews <ben.matthews@seccl.tech>
2021-02-21 13:38:31 +0000

    adding new handlers

--- app.ts
+++ app.ts
@@ -4,11 +4,30 @@ import awsServerlessExpress from 'aws-serverless-express'
 export let app = express.default()
 app.use(express.json())
 
-app.use('/', (req: express.Request, res: express.Response) => {
+app.use('/action', (req: express.Request, res: express.Response) => {
+  console.log(req)
+  res.status(200)
+  res.send('Hello from the action API')
+})
+
+app.use('/authenticate', (req: express.Request, res: express.Response) => {
+  console.log(req)
+  res.status(200)
+  res.send('Hello from the authentication API')
+})
+
+app.use('/register', (req: express.Request, res: express.Response) => {
+  console.log(req)
+  res.status(200)
+  res.send('Hello from the registration API')
+})
+
+app.use('/v1', (req: express.Request, res: express.Response) => {
   console.log(req)
   res.status(200)
   res.send('Hello from the API')
 })
+
 
 const server = awsServerlessExpress.createServer(app, undefined)
 export const handler = (event: any, context: any) => awsServerlessExpress.proxy(server, event, context)
