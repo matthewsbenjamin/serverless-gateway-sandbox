9f377a4e093528e0f8859cbb88f0f9b3c67662d3
Ben Matthews <ben.matthews@seccl.tech>
2021-02-21 12:39:06 +0000

    adding TS

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
-import * as express from 'express'
-import awsServerlessExpress from 'aws-serverless-express'
-
-export let app = express.default()
-app.use(express.json())
-
-app.use('/', (req, res) => {
-  console.log(req)
-  res.status(200)
-  res.send('Hello from the API')
-})
-
-const server = awsServerlessExpress.createServer(app, undefined)
-export const handler = (event, context) => awsServerlessExpress.proxy(server, event, context)
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.handler = exports.app = void 0;
+var express = __importStar(require("express"));
+var aws_serverless_express_1 = __importDefault(require("aws-serverless-express"));
+exports.app = express.default();
+exports.app.use(express.json());
+exports.app.use('/', function (req, res) {
+    console.log(req);
+    res.status(200);
+    res.send('Hello from the API');
+});
+var server = aws_serverless_express_1.default.createServer(exports.app, undefined);
+var handler = function (event, context) { return aws_serverless_express_1.default.proxy(server, event, context); };
+exports.handler = handler;
